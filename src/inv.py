42d178c5ba027da0c983e876b676d99f639205c8
kcaisley <kcaisley@uni-bonn.de>
2023-07-14 13:58:42 +0200

    Copying in existing work

---
+++ src/inv.py
@@ -0,0 +1,21 @@
+import hdl21 as h
+
+# Generator for basic CMOS inverters. Various sizes and scales are needed.
+
+@h.paramclass
+class InvParams:
+    lengths = h. Param(dtype=int, desc="W")
+    widths = h.Param(dtype=int, desc="")
+    pnratio = h.Param(dtype=int, desc="Ratio of PMOS to NMOS widths. Required.") 
+
+
+#Params
+@h.generator
+def InvGenerator(params: MyParams) -> h.Module:
+
+
+class Inv:
+    in = h.Input()
+    out = h.Output()
+    vdd = h.Power()
+    vss = h.Ground()
