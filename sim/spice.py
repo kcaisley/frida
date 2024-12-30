17030886a55c98c058dc41eb83e5c940d6c3ffb1
Kennedy Caisley <kcaisley@uni-bonn.de>
2024-12-30 14:56:55 -0800

    fixed Pyright errors using df.loc()

--- sim/spice.py
+++ sim/spice.py
@@ -1,76 +1,73 @@
 # from sar_adc_sim import *
 import matplotlib.pyplot as plt
 import pandas as pd
-# import numpy as np
 import os
-os.environ["XDG_SESSION_TYPE"] = "xcb" # this silences the
+os.environ["XDG_SESSION_TYPE"] = "xcb" # this silences the display Wayland error
 
-radix = 2.0     #
+radix = 1.8     #
 convs = 8       # how many conversion registers are set each
-duration = 300  # in microseconds
+duration = 1100  # in microseconds
 vhigh = 1.2  # voltages for low and high logic levels
 
 # Load the CSV data into a pandas DataFrame, skip first row as it just has text e.g. "Transient analysis: temperature=25.0"
-raw = pd.read_csv(f'{radix}radix_{convs}bit_{duration}u.csv', skiprows=1)
+df = pd.read_csv(f'{radix}radix_{convs}bit_{duration}u.csv', skiprows=1)
 
 # Remove unwanted parts from column names: V(....)
-raw.columns = raw.columns.str.replace(r'V\(|\)', '', regex=True)
+df.columns = df.columns.str.replace(r'V\(|\)', '', regex=True)
 
 # We have since sim time step is 0.1ns, and ADC runs on 10 MHz clock (100ns), we have a valid code each 1000 rows (with some offset, 250)
-raw = raw[(raw.index % 1000) == 250 ]
-raw = raw.reset_index(drop=True)
+df = df[(df.index % 1000) == 250 ]
+df = df.reset_index(drop=True)
 sample_count = duration * 10 # this should equand we have a ADC conversion
 
-# The voltages aren't available until the
+# The voltages aren't available until the second
 for col in ['Time','reference', 'signal', 'cnode3p', 'cnode3n']:
-    raw[col] = raw[col].shift(1)
+    df.loc[:, col] = df.loc[:, col].shift(1)
 
 # Remove first two rows, as it is from the power on cycle
-raw = raw.drop(index=[0, 1]).reset_index(drop=True)
+df = df.drop(index=[0, 1]).reset_index(drop=True)
 
 # Remove every row past 580, as the ADC stops acting linear due to switch
-# raw = raw.iloc[:7000].reset_index(drop=True)
+# df = df.iloc[:7000].reset_index(drop=True)
 
 # Delete the clock columns, as we don't need them
-raw = raw.drop(columns='syncp')
-raw = raw.drop(columns='clockp')
+df = df.drop(columns='syncp')
+df = df.drop(columns='clockp')
 
 # Find total input, drop unneeded values
-raw['Vin'] = raw['reference'] - raw['signal']
-raw = raw.drop(columns='signal')
-raw = raw.drop(columns='reference')
+df['Vin'] = df['reference'] - df['signal']
+df = df.drop(columns='signal')
+df = df.drop(columns='reference')
 
 # Find real input, drop unneeded values
-raw['Vsamp'] = raw['cnode3n'] - raw['cnode3p']
-# raw = raw.drop(columns='cnode3p')
-# raw = raw.drop(columns='cnode3n')
+df['Vsamp'] = df['cnode3n'] - df['cnode3p']
+# df = df.drop(columns='cnode3p')
+# df = df.drop(columns='cnode3n')
 
 # Digitize the data bit lines to that they are either 1 or 0
 for col in [f'data<{i}>' for i in range(convs)]:  # Create list ['data<0>', 'data<1>', ..., 'data<7>']
-    raw[col] = raw[col].apply(lambda x: 1 if x > 1.1 else (0 if x < 0.1 else x))
-
-
+    df[col] = df.loc[:,col].apply(lambda x: 1 if x > 1.1 else (0 if x < 0.1 else x))
 
 # Define the binary weights for data<0> to data<7>, where data<0> is the LSB, and data<7> is MSB
-weights = [1/(radix**i) for i in reversed(range(convs))]
+weights = [radix**i for i in reversed(range(convs))]
 print(weights)
 
-# Calculate the 'Vout' column
-# The 'Vout' column contains the weighted sum of data<0> to data<7>
-raw['Vout'] = sum((raw[f'data<{i}>'] * vhigh * weight) for i, weight in enumerate(weights))
+# Calculate the 'Dout' column
+# The 'Dout' column contains the weighted sum of data<0> to data<7>
+df['Dout'] = sum(df[f'data<{7-i}>'] * weight for i, weight in enumerate(weights))
 
-# Plot the line for Vin vs Vout
-plt.plot(raw['Vin'], raw['Vout'], label='Vin vs Vout', color='b')
+# Plot the line for Vin vs Dout
+plt.plot(df['Vin'], df['Dout'], label='Dout vs Vin', color='b')
 
 # Adding labels and title
 plt.xlabel('Vin')
-plt.ylabel('Vout')
-plt.title(f'Vout vs Re-analog (radix = {radix}, conversions = {convs}, samples = {duration*10})')
+plt.ylabel('Dout')
+plt.title(f'Dout vs Vin (radix = {radix}, conversions = {convs}, samples = {duration*10})')
 
 # Show the plot
 plt.legend()
 plt.grid(True)
-# plt.show()
+plt.show()
 
 # Slice the DataFrame to exclude the first and last 100 rows
 
@@ -78,7 +75,7 @@ plt.grid(True)
 plt.figure(figsize=(10, 6))
 
 histbins = list(range(0, 256))
-plt.hist(raw['Vout'], bins=histbins, color='g', edgecolor='black')
+plt.hist(df['Dout'], bins=histbins, color='g', edgecolor='black')
 
 # Adding labels and title
 plt.xlabel('Re-Analog')
@@ -87,4 +84,4 @@ plt.title(f'Re-Analog Code Density (radix = {radix}, conversions = {convs}, samp
 
 # Show the plot
 plt.grid(True)
-# plt.show()
+plt.show()
