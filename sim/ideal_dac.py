2bbd70c8e901a259128eeeed238520f4ccd14a9e
Kennedy Caisley <kcaisley@uni-bonn.de>
2025-01-24 22:14:41 +0100

    clean up simulation

--- sim/ideal_dac.py
+++ sim/ideal_dac.py
@@ -1,21 +1,14 @@
-binary_list = [[1 if digit & (1 << j) else 0 for j in range(8)] for digit in range(256)]
-weights = [1.8**i for i in range(8)]
-dout = [sum((2*binary_list[k][i]-1) * weight for i, weight in enumerate(weights)) for k in range(256)]
+import matplotlib.pyplot as plt
 
-# more verbose
-# dout = []
-# for k in range(256):
-#     sum_value = 0
-#     for i, weight in enumerate(weights):
-#         bit = binary_list[k][i]
-#         print(bit)
-#         sum_value += (2*bit - 1) * weight # converts 0 to -1, 1 to 1
-#     dout.append(sum_value)
+bits = 9
+radix = 1.8
 
-print(binary_list)
-print(dout)
+# produces a list of lists with binary codes. For 8 bit example [[0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0]....
+binary_list = [[1 if digit & (1 << j) else 0 for j in range(bits)] for digit in range(2**bits)]
+weights = [radix**i for i in range(bits)]
+dout = [sum((2*binary_list[k][i]-1) * weight for i, weight in enumerate(weights)) for k in range(2**bits)]
 
-print("okay, sorting now....")
+# We want the output DAC values to be sorted by magnitude, and we want to have an their associated binary codes.
 # Zips dout and binary_list together.
 # Sorts the pairs based on the first element of each tuple (dout value).
 # Unzips the sorted pairs back into dout and binary_list.
@@ -25,15 +18,7 @@ dout_sorted, binary_list_sorted = list(dout_sorted), list(binary_list_sorted)
 print(dout_sorted)
 print(binary_list_sorted)
 
-
-print("comparing....")
-print(dout_sorted == dout)
-print(binary_list_sorted == binary_list)
-
-
-import matplotlib.pyplot as plt
-
-# Assuming dout and dout_sorted are already defined and have 256 values
+# Assuming dout and dout_sorted are already defined and have 2**bits values
 indices = range(len(dout))
 
 plt.step(indices, dout, label='dout', color='blue')
@@ -41,7 +26,7 @@ plt.step(indices, dout_sorted, label='dout_sorted', color='red')
 
 plt.xlabel('Index')
 plt.ylabel('Value')
-plt.title('Comparison of dout and dout_sorted')
+plt.title(f'Comparison of dout and dout_sorted (radix = {radix}, bits = {bits})')
 plt.legend()
 
 plt.show()
