7e9cd70892485d514c612c8f7d3df843906bfae2
Kennedy Caisley <kcaisley@uni-bonn.de>
2025-06-13 20:46:05 +0200

    factoring out OA files, as these will be only input and output collateral

---
+++ tech/tsl018/workspace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /eda/local/scripts/sos_7.20.p1.sh
+source /eda/local/scripts/cadence_2024-25.sh
+source /eda/local/scripts/siemens_2024-25.sh
+source /eda/local/scripts/tj_180_ts18is_6.3.6.sh
