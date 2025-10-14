82c16e4f6752527ca98c1cb42595588803f215f1
kcaisley <kcaisley@uni-bonn.de>
2025-10-14 21:09:50 +0200

    Filler cells aren't electrical, remove them from netlist

--- design/tsmc65/clean_cdl.py
+++ design/tsmc65/clean_cdl.py
@@ -110,7 +110,7 @@ def clean_cdl_text(cdl_text):
     # Step 1: Remove filler and decap instances
     # Filter out lines matching XFILLER with any of the filler/decap cell types
     # DECAP LIST: |DCAPLVT|DCAP4LVT|DCAP8LVT|DCAP16LVT|DCAP32LVT|DCAP64LVT|FILL1LVT
-    filler_pattern = r'^XFILLER.*(FILL0LVT).*$'
+    filler_pattern = r'^XFILLER.*(FILL1LVT).*$'
     lines = cdl_text.split('\n')
     filtered_lines = [line for line in lines if not re.match(filler_pattern, line)]
     cdl_text = '\n'.join(filtered_lines)
