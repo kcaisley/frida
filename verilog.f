c3cee2a4fc7a8103fbfbaef393bdf946170ed700
Kennedy Caisley <kcaisley@uni-bonn.de>
2026-09-18 22:51:11 +0200

    fix(editor): select BDAQ53 primitive models for HDL analysis
    Exclude conflicting Spartan DDR wrappers from the Slang index and match the BDAQ53 build define.

--- verilog.f
+++ verilog.f
@@ -1,4 +1,7 @@
 -F libs/basil/verilog.f
+// Select the ODDR model instead of the Spartan wrappers with the same name.
+-v libs/basil/basil/firmware/modules/utils/ODDR.v
++define+BDAQ53=1
 +incdir+design/hdl
 +incdir+design/fpga
 +incdir+design/fpga/SiTCP
